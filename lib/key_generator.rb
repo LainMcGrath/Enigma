840f7b43cc824f864c9d82496b6fcc9382f2cdf4
Lain McGratH <lain.mcgrath@gusto.com>
2019-11-02 15:03:33 -0600

    Edit initialization to generate random key

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -1,27 +1,24 @@
+require 'Date'
 class KeyGenerator
 
   def initialize
+    @generated_key = rand(00000..99999)
   end
-
-  def generate_random_key
-    5.times.map {rand(9)}.join
-  end
-
+  
   def slice_random_key
-    r_key = generate_random_key
     random_key = []
-    random_key << r_key.to_s
+    random_key << @generated_key.to_s
     new = random_key[0].split("")
   end
 
   def random_key_offsets
     random = slice_random_key
-    random.reduce({}) do |alp, number|
-      alp[:a] = ((random[0] + random[1]).to_i)
-      alp[:b] = ((random[1] + random[2]).to_i)
-      alp[:c] = ((random[2] + random[3]).to_i)
-      alp[:d] = ((random[3] + random[4]).to_i)
-      alp
+    random.reduce({}) do |acc, number|
+      acc[:a] = ((random[0] + random[1]).to_i)
+      acc[:b] = ((random[1] + random[2]).to_i)
+      acc[:c] = ((random[2] + random[3]).to_i)
+      acc[:d] = ((random[3] + random[4]).to_i)
+      acc
     end
   end
 end
