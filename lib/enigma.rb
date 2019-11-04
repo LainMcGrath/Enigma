7cbff50dd19b0545efcecdc08de01a0e5177ee27
Lain McGratH <lain.mcgrath@gusto.com>
2019-11-04 16:58:32 -0700

    Add engima class and test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,40 +1,6 @@
 class Enigma
 
-  attr_reader :character_set, :offset, :key_generator
-
   def initialize
-    @character_set = ("a".."z").to_a << " "
-    @offset = Offset.new
-    @key_generator = KeyGenerator.new
-  end
-
-  def shift_amount
-    offset = @offset.offset_amount
-    key_gen = @key_generator.random_key_sets
-    a = offset[0] + key_gen[0]
-    b = offset[1] + key_gen[1]
-    c = offset[2] + key_gen[2]
-    d = offset[3] + key_gen[3]
-    shifts = [a, b, c, d]
-  end
-
-  def rotate_shift_amount
-    shift_amount.rotate!
-  end
-
-  def characters_to_ord
-    @character_set.map { |letter| letter.ord }.rotate!
-  end
-
-  def message_encrypt(message, key)
-    message_to_ord = message.downcase.chars.map { |letter| letter.ord }
-    shifted_message = message_to_ord.map do |letter|
-      if 
-      letter + key
-      require "pry"; binding.pry
-    end
-    joined_message = shifted_message.map { |letter| letter.chr }.join
-      require "pry"; binding.pry
   end
 
   def encrypt(message)
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,37 +1,22 @@
 require './test/test_helper'
+require './lib/encrypting'
 require './lib/enigma'
 require './lib/offsets'
 require './lib/key_generator'
 
-class EnigmaTest < Minitest::Test
+class EncryptTest < Minitest::Test
 
   def setup
     @enigma = Enigma.new
   end
 
-  def test_it_exists
-    assert_instance_of Enigma, @enigma
-  end
-
-  def test_it_has_attributes
-    placeholder = ["a", "b", "c", "d", "e", "f",
-                    "g", "h", "i", "j", "k", "l",
-                    "m", "n", "o", "p", "q", "r",
-                    "s", "t", "u", "v", "w",
-                    "x", "y", "z", " "]
-    assert_equal placeholder, @enigma.character_set
-    assert_instance_of Offset, @enigma.offset
-    assert_instance_of KeyGenerator, @enigma.key_generator
-  end
-
-  # def test_it_can_calculate_shift_amount
-  #   placeholder = {:a=>27, :b=>51, :c=>14, :d=>89}
-  #   # @enigma.expects(:shift_amount).returns({:a=>27, :b=>51, :c=>14, :d=>89})
-  #   assert_equal placeholder, @enigma.shift_amount
-  # end
-
-  def test_it_can_encrypt
-    # assert_instance_of Hash, @enigma.encrypt("Hello world!", 1)
-    assert_equal "mjqqt", @enigma.message_encrypt("Hello", 62)
+#test doesn't pass but that is because I don't know how to use stubs
+  def test_it_can_create_encrypt
+    placeholder = {
+      :encryption => "kbtfr",
+      :key        => 8763,
+      :date       => 041119
+    }
+    assert_equal placeholder, @enigma.encrypt("hello")
   end
 end
