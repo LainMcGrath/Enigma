bac3d8a59349bf9b4a67aff4916a26f53e53ec8d
Lain McGratH <lain.mcgrath@gusto.com>
2019-11-05 13:27:32 -0700

    Refactored to pass in key and date in encrypt and decrypt methods

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,13 +1,29 @@
-class Enigma
+require 'Date'
+require './lib/encrypted'
+require './lib/decrypted'
+require './lib/key_generator'
 
+class Enigma
   def initialize
+    @key_generator = KeyGenerator.new.slice_random_key.join("")
+    @date = Time.now.strftime("%d%m%y")
   end
 
-  def encrypt(message)
+  def encrypt(message, key = @key_generator, date = @date)
+    create_encryption = Encrypted.new(key, date)
     encryption_list = {}
-    encryption_list[:encryption] = message
-    encryption_list[:key] = @key_generator.generated_key
-    encryption_list[:date] = @offset.find_date
+    encryption_list[:encryption] = create_encryption.encrypt_message(message)
+    encryption_list[:key] = key
+    encryption_list[:date] = date
     encryption_list
+  end
+
+  def decrypt(message, key, date = @date)
+    create_decryption = Decrypted.new(key, date)
+    decryption_list = {}
+    decryption_list[:decryption] = create_decryption.decrypt_message(message)
+    decryption_list[:key] = key
+    decryption_list[:date] = date
+    decryption_list
   end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,5 +1,6 @@
 require './test/test_helper'
-require './lib/encrypting'
+require './lib/encrypted'
+require './lib/decrypted'
 require './lib/enigma'
 require './lib/offsets'
 require './lib/key_generator'
@@ -10,13 +11,33 @@ class EncryptTest < Minitest::Test
     @enigma = Enigma.new
   end
 
+  def test_it_exists
+    assert_instance_of Enigma, @enigma
+  end
+
+  def test_it_has_attributes
+    assert_instance_of KeyGenerator, @enigma.key_generator
+    assert_instance_of Offset, @enigma.offset
+    assert_instance_of Encrypted, @enigma.encrypted
+  end
+
+
 #test doesn't pass but that is because I don't know how to use stubs
   def test_it_can_create_encrypt
     placeholder = {
       :encryption => "kbtfr",
-      :key        => 8763,
-      :date       => 041119
+      :key        => "98958",
+      :date       => "541119"
     }
     assert_equal placeholder, @enigma.encrypt("hello")
+  end
+
+  def test_it_can_dencrypt
+    placeholder = {
+      :encryption => "hello!",
+      :key        => "98958",
+      :date       => "541119"
+    }
+    assert_equal placeholder, @enigma.decrypt("mabmt")
   end
 end
