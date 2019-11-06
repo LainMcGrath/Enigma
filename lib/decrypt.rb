353365b59888f89de1cb221782ac83099cab69db
Lain McGratH <lain.mcgrath@gusto.com>
2019-11-05 22:57:13 -0700

    Add not functioning decrypt

---
+++ lib/decrypt.rb
@@ -0,0 +1,19 @@
+# require './lib/key_generator'
+require './lib/enigma'
+require './lib/decrypted'
+require './lib/encrypt'
+require 'date'
+
+enigma = Enigma.new
+key_gens = enigma.key_generator
+handle = File.open("encrypted.txt", "r")
+message_to_decrypt = handle.read
+handle.close
+
+decrypted_message = enigma.decrypt(message_to_decrypt, key_gens, enigma.date)
+writer = File.open("decrypted.txt", "w")
+writer.write(decrypted_message[:decryption])
+writer.close
+
+
+puts "Created 'decrypted.txt' with the key #{key_gens} and date #{enigma.date}."
