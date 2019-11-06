eef804ed379437135a893068a1da04c1c76994a5
Lain McGratH <lain.mcgrath@gusto.com>
2019-11-06 07:40:35 -0700

    Add functioning decryptgit add lib/decrypt.rb git add lib/decrypt.rb

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -10,7 +10,7 @@ handle = File.open("encrypted.txt", "r")
 message_to_decrypt = handle.read
 handle.close
 
-decrypted_message = enigma.decrypt(message_to_decrypt, key_gens, enigma.date)
+decrypted_message = enigma.decrypt(message_to_decrypt, ARGV[2], ARGV[3])
 writer = File.open("decrypted.txt", "w")
 writer.write(decrypted_message[:decryption])
 writer.close
